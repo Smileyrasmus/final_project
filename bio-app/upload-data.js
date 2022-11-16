30b5ea05486de834b547f2427818aff9731161fd
Rasmus Emil Bøgel Larsen <rebl@wexo.dk>
2022-11-16 13:11:23 +0100

    added auth to solid js project!!!!!!!!

--- bio-app/upload-data.js
+++ bio-app/upload-data.js
@@ -1,29 +1,41 @@
 
 const axios = require('axios');
 
+const config = {
+  headers: {
+    Authorization: "Token 741149ee55478c463eff3248d9c9a0389c38a82b"
+  }
+}
+
 const data = {
 
 }
 
 // axios post request
-axios
-.post('http://localhost:3000/api/upload', data)
-.then((res) => {
-  console.log(res.data),
-  console.log(res.status)
-})
-.catch((err) => { 
-  console.log(err) 
-});
+function post1() {
+  axios
+  .post('http://localhost:3000/api/upload', data)
+  .then((res) => {
+    console.log(res.data),
+    console.log(res.status)
+  })
+  .catch((err) => { 
+    console.log(err) 
+  });
+}
 
 // axios get request
-axios
-.get('http://localhost:8000/bookableitems')
-.then(res => {
-  console.log(`statusCode: ${res.statusCode}`)
-  // console.log(res.data)
-}) 
-.catch(error => {
-  console.error(error)
-})
+function get1() {
+  axios
+  .get('http://localhost:8000/bookableitems', config)
+  .then(res => {
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(res.data)
+  }) 
+  .catch(error => {
+    console.error(error)
+  })
+}
+
+get1()
 
