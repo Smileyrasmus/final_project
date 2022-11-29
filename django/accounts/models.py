b2020e518eb41ba75871130b6f52fe36b2f85aa0
Vigtor <vigtorbr@gmail.com>
2022-11-29 10:03:17 +0100

    Naming

--- django/accounts/models.py
+++ django/accounts/models.py
@@ -7,10 +7,10 @@ import uuid
 def default_conditions():
     return dict(
         order=dict(must_have_customer_id=True),
-        booking=dict(dublicate_booking=True, must_have_bookable_item=False),
-        event=dict(start_end_overlay=True, dublicate_event=True),
-        location=dict(dublicate_location=True),
-        bookable_item=dict(dublicate_bookable_item=True, active=True),
+        booking=dict(duplicate_booking=True, require_bookable_item=False),
+        event=dict(start_end_overlay=True, duplicate_event=True),
+        location=dict(duplicate_location=True),
+        bookable_item=dict(duplicate_bookable_item=True),
     )
 
 
