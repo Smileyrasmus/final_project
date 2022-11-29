6ec2de521b1ad36f448f37b1d5363a538035e427
Rasmus Emil Bøgel Larsen <1086243@ucn.dk>
2022-11-29 08:17:00 +0100

    Auth tokens are created for new users

--- django/accounts/views.py
+++ django/accounts/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
 from django.contrib.auth.models import Group
@@ -7,6 +6,11 @@ from .serializers import UserSerializer, GroupSerializer
 
 
 from rest_framework.permissions import IsAuthenticated
+
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -17,6 +21,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+    # auto create token on user creation
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def create_auth_token(sender, instance=None, created=False, **kwargs):
+        if created:
+            Token.objects.create(user=instance)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
