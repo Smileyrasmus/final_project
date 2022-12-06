f7f623fe774262fce64e9d93936b1b50a66fbd62
Rasmus Emil Bøgel Larsen <1086243@ucn.dk>
2022-12-06 11:45:41 +0100

    trying ones more

--- django/bookingapi/views.py
+++ django/bookingapi/views.py
@@ -103,7 +103,7 @@ class OrderViewSet(BaseViewSet):
             )
         return super().get_queryset()
 
-    # @transaction.atomic
+    @transaction.atomic
     def create(self, request, *args, **kwargs):
         if not request.data.get("bookings"):
             return Response(
@@ -111,27 +111,26 @@ class OrderViewSet(BaseViewSet):
                 status=status.HTTP_400_BAD_REQUEST,
             )
         self.queryset = Order.objects.select_for_update().all()
+
         order_serializer = self.get_serializer(data=request.data["order"])
-        headers = None
-        with transaction.atomic():
-            order_serializer.is_valid(raise_exception=True)
-            self.perform_create(order_serializer)
-            order_headers = self.get_success_headers(order_serializer.data)
+        order_serializer.is_valid(raise_exception=True)
+        self.perform_create(order_serializer)
+        order_headers = self.get_success_headers(order_serializer.data)
 
-            # create bookings with order id
-            id = order_serializer.data["id"]
-            bookings = request.data["bookings"]
-            for booking in bookings:
-                booking["order"] = id
+        # create bookings with order id
+        id = order_serializer.data["id"]
+        bookings = request.data["bookings"]
+        for booking in bookings:
+            booking["order"] = id
 
-            self.serializer_class = BookingSerializer
-            booking_serializer = self.get_serializer(data=bookings, many=True)
-            booking_serializer.is_valid(raise_exception=True)
-            self.perform_create(booking_serializer)
-            booking_headers = self.get_success_headers(booking_serializer.data)
+        self.serializer_class = BookingSerializer
+        booking_serializer = self.get_serializer(data=bookings, many=True)
+        booking_serializer.is_valid(raise_exception=True)
+        self.perform_create(booking_serializer)
+        booking_headers = self.get_success_headers(booking_serializer.data)
 
-            # merge 2 dicts
-            headers = order_headers.update(booking_headers)
+        # merge 2 dicts
+        headers = order_headers.update(booking_headers)
 
         return Response(
             order_serializer.data,
